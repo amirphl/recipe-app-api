1642e001e7efc4cff706e02fde32ec732c887a83
amirphl <amirphl4@gmail.com>
2020-02-24 20:05:04 +0330

    Add migrations for User model in core app

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
+
 # Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **kwargs):
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
--- x
+++ x
@@ -4,3 +4,4 @@ docker-compose build
 docker-compose run app sh -c "django-admin.py startproject app ."
 docker-compose run app sh -c "python manage.py test && flake8"
 docker-compose run app sh -c "python manage.py startapp core"
+docker-compose run app sh -c "python manage.py makemigrations"
